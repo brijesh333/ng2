101a0e01b07a21882199e0c540fd4bee72f88269
Brijesh <patelbrijesh333@gmail.com>
2017-09-18 11:19:09 +0530

    sample UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
 
