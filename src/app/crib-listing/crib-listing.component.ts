7d5bdc54a144680fd567363b36de2c0a984b3c77
Brijesh <patelbrijesh333@gmail.com>
2017-09-20 00:51:39 +0530

    HTTP call

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { cribs } from './../data/cribs';
+//import { cribs } from './../data/cribs';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
-  selector: 'app-crib-listing',
-  templateUrl: './crib-listing.component.html',
-  styleUrls: ['./crib-listing.component.css']
+    selector: 'app-crib-listing',
+    templateUrl: './crib-listing.component.html',
+    styleUrls: ['./crib-listing.component.css']
 })
 export class CribListingComponent implements OnInit {
 
-  cribs: Array<any>=cribs;
+    cribs: Array<any>;
+    error: string;
+    constructor(private http: Http) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.http.get('/data/cribs.json')
+            .map(res => res.json())
+            .subscribe(
+            data => this.cribs = data,
+            error => this.error = error
+      );
+    }
 
 }
